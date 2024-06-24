c9c72922add0efcc1825e58c579591bcd0835622
NigthDreamer <migthdreams@gmail.com>
2024-06-24 19:10:49 +0200

    Se elimina la configuracion previa de los service workers

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { CalendarApp } from './CalendarApp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import './registerServiceWorker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		VitePWA({
-			registerType: 'prompt',
-			injectRegister: null,
-
-			pwaAssets: {
-				disabled: false,
-				config: true,
-			},
-
-			manifest: {
-				name: 'Calendar',
-				short_name: 'Calendar',
-				description: 'Aplicacion del calendario',
-				theme_color: '#ffffff',
-			},
-
-			// workbox: {
-			// 	globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-			// 	cleanupOutdatedCaches: true,
-			// 	clientsClaim: true,
-			// },
-
-			// devOptions: {
-			// 	enabled: false,
-			// 	navigateFallback: 'index.html',
-			// 	suppressWarnings: true,
-			// 	type: 'module',
-			// },
-		}),
-	],
+	plugins: [react()],
 });
