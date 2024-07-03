e05941c6f6391e84139be747c502bad44357cc2b
NigthDreamer <migthdreams@gmail.com>
2024-07-03 21:32:05 +0200

    Se implementa el vitePWA completamente

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,30 +1,30 @@
-import { useAuthStore } from "../../hooks"
+import { useAuthStore } from '../../hooks';
 
 import { Online, Offline } from 'react-detect-offline';
 
 export const Navbar = () => {
-  const { startLogout, user } = useAuthStore();
+	const { startLogout, user } = useAuthStore();
 
-  return (
-    <div className="navbar navbar-dark bg-dark mb-4 px-4">
-      <span className="navbar-brand">
-        <i className="fas fa-calendar-alt"></i>
-        &nbsp;
-        {user.name}
-      </span>
+	return (
+		<div className="navbar navbar-dark bg-dark mb-4 px-4">
+			<span className="navbar-brand">
+				<i className="fas fa-calendar-alt"></i>
+				&nbsp;
+				{user.name}
+			</span>
 
-      <Online>
-        Online
-      </Online>
-      <Offline>
-        Offline - Las peticiones serán guardadas
-      </Offline>
+			<Online>
+				<span className="text-success">Online</span>
+			</Online>
+			<Offline>
+				<span className="text-danger"> Offline - Las peticiones serán guardadas</span>
+			</Offline>
 
-      <button className="btn btn-outline-danger" onClick={startLogout}>
-        <i className="fas fa-sign-out-alt"></i>
-        &nbsp;
-        <span>Salir</span>
-      </button>
-    </div>
-  )
-}
+			<button className="btn btn-outline-danger" onClick={startLogout}>
+				<i className="fas fa-sign-out-alt"></i>
+				&nbsp;
+				<span>Salir</span>
+			</button>
+		</div>
+	);
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,119 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+      manifest: {
+        name: 'Calendar',
+        short_name: 'Calendar',
+        description: 'Aplicacion del calendario',
+        theme_color: '#ffffff',
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        runtimeCaching: [
+          {
+            urlPattern: /https:\/\/kit\.fontawesome\.com\/\.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'fontawesome-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: /https:\/\/mern-calendar-nigthdreamer-3e02285419bf\.herokuapp\.com\/api\/auth\/renew/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'renew-network-first',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              },
+            }
+          },
+          {
+            urlPattern: /https:\/\/mern-calendar-nigthdreamer-3e02285419bf\.herokuapp\.com\/api\/events/i,
+            handler: 'NetworkFirst',
+            method: 'GET',
+            options: {
+              cacheName: 'events-network-first',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              },
+            }
+          },
+          {
+            handler: 'NetworkOnly',
+            urlPattern: /https:\/\/mern-calendar-nigthdreamer-3e02285419bf\.herokuapp\.com\/api\/events/i,
+            method: 'POST',
+            options: {
+              backgroundSync: {
+                name: 'offlinePostsQueue',
+                options: {
+                  maxRetentionTime: 24 * 60
+                }
+              }
+            }
+          },
+          {
+            handler: 'NetworkOnly',
+            urlPattern: /https:\/\/mern-calendar-nigthdreamer-3e02285419bf\.herokuapp\.com\/api\/events/i,
+            method: 'PUT',
+            options: {
+              backgroundSync: {
+                name: 'offlinePutsQueue',
+                options: {
+                  maxRetentionTime: 24 * 60
+                }
+              }
+            }
+          },
+          {
+            handler: 'NetworkOnly',
+            urlPattern: /https:\/\/mern-calendar-nigthdreamer-3e02285419bf\.herokuapp\.com\/api\/events/i,
+            method: 'DELETE',
+            options: {
+              backgroundSync: {
+                name: 'offlineDeletesQueue',
+                options: {
+                  maxRetentionTime: 24 * 60
+                }
+              }
+            }
+          },
+        ]
+      },
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
 });
